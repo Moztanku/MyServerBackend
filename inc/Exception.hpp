c9875840d3b98bfdb69542d8284190a67e05de2f
Moztanku <Mostankpl@gmail.com>
2024-05-27 18:25:07 +0200

    Add listener

--- inc/Exception.hpp
+++ inc/Exception.hpp
@@ -1,17 +1,40 @@
 #pragma once
 
+#include <source_location>
 #include <iostream>
 #include <format>
 
-enum class exception_type
+enum class exception_type : int
 {
-    file_not_found,
-    invalid_config_file,
-    unnamed_yet
+    unnamed_yet = 0,
+    file_not_found = 1,
+    invalid_config_file = 2
 };
 
-void throw_exception(exception_type type, const std::string& message = "")
+auto to_string(exception_type type) -> std::string
 {
-    std::cout << std::format("Exception [{}]: {}\n", static_cast<int>(type), message);
+    switch (type)
+    {
+    case exception_type::unnamed_yet:
+        return "unnamed_yet";
+    case exception_type::file_not_found:
+        return "file_not_found";
+    case exception_type::invalid_config_file:
+        return "invalid_config_file";
+    default:
+        return "unknown";
+    }
+}
+
+void throw_exception(exception_type type, const std::string& message = "", const std::source_location& location = std::source_location::current())
+{
+    constexpr std::string_view dark = "\033[38;5;8m";
+    constexpr std::string_view reset = "\033[0m";
+
+    std::cout << std::format(
+        "Exception [{}]: {} {} @ {}:{}{}\n", 
+        to_string(type), message,
+        dark, location.file_name(), location.line(), reset
+    );
     std::exit(EXIT_FAILURE);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,117 @@
 #include "Startup.hpp"
 
 namespace net = boost::asio;
+namespace beast = boost::beast;
+
 using tcp = net::ip::tcp;
+
+class Session : public std::enable_shared_from_this<Session>
+{
+    public:
+        explicit Session(
+            tcp::socket&& socket,
+            net::ssl::context& ctx) : m_stream{std::move(socket), ctx}
+        {}
+
+        void run()
+        {
+            // Time out after 30 seconds
+            beast::get_lowest_layer(m_stream).expires_after(std::chrono::seconds(30));
+
+            // Perform the SSL handshake
+            m_stream.async_handshake(
+                net::ssl::stream_base::server,
+                beast::bind_front_handler(
+                    &Session::on_handshake,
+                    shared_from_this()
+                )
+            );
+        }
+    private:
+        beast::ssl_stream<beast::tcp_stream> m_stream;
+
+        void on_handshake(beast::error_code ec)
+        {
+            if (ec)
+                throw_exception(exception_type::unnamed_yet, ec.message());
+
+            do_read();
+        }
+
+        void do_read()
+        {
+        }
+
+};
+
+class Listener : public std::enable_shared_from_this<Listener>
+{
+    public:
+        Listener(
+            net::io_context& ioc,
+            net::ssl::context& ctx,
+            tcp::endpoint endpoint) : m_ioc{ioc}, m_ctx{ctx}, m_acceptor{ioc}
+        {
+            beast::error_code ec;
+
+            // Open the acceptor
+            m_acceptor.open(endpoint.protocol(), ec);
+            if (ec)
+                throw_exception(exception_type::unnamed_yet, ec.message());
+
+            // Allow address reuse
+            m_acceptor.set_option(net::socket_base::reuse_address(true), ec);
+            if (ec)
+                throw_exception(exception_type::unnamed_yet, ec.message());
+
+            // Bind to the server address
+            m_acceptor.bind(endpoint, ec);
+            if (ec)
+                throw_exception(exception_type::unnamed_yet, ec.message());
+
+            // Start listening for connections
+            m_acceptor.listen(net::socket_base::max_listen_connections, ec);
+            if (ec)
+                throw_exception(exception_type::unnamed_yet, ec.message());
+        }
+
+        void run()
+        {
+            if (!m_acceptor.is_open())
+                return;
+
+            do_accept();
+        }
+
+    private:
+        net::io_context& m_ioc;
+        net::ssl::context& m_ctx;
+        tcp::acceptor m_acceptor;
+
+        void do_accept()
+        {
+            m_acceptor.async_accept(
+                net::make_strand(m_ioc),
+                beast::bind_front_handler(
+                    &Listener::on_accept,
+                    shared_from_this()
+                )
+            );
+        }
+
+        void on_accept(beast::error_code ec, tcp::socket socket)
+        {
+            if (ec)
+                throw_exception(exception_type::unnamed_yet, ec.message());
+
+            std::make_shared<Session>(
+                 std::move(socket), m_ctx
+            )->run();
+
+            do_accept();
+        }
+
+};
 
 int main()
 {
@@ -20,60 +130,38 @@ int main()
     const net::ip::address_v4& address = config_file.address;
     const uint16_t& port = config_file.port;
 
-    auto ssl_ctx = getSSLContext(
+    net::ssl::context ssl_ctx = getSSLContext(
         config_file.ssl_key_path,
         config_file.ssl_cert_path,
         config_file.ssl_dh_path,
         config_file.ssl_passphrase
     );
 
-    uint thread_count = 1u; // for now
+    uint thread_count = 4u; // for now
 
     if (thread_count == 0 || thread_count > std::thread::hardware_concurrency())
         throw_exception(exception_type::unnamed_yet, "Invalid thread count");
 
     net::io_context ioc{static_cast<int>(thread_count)};
-    tcp::acceptor acceptor{ioc, {address, port}};
+    // tcp::acceptor acceptor{ioc, {address, port}};
 
     std::cout << "Server started on " << address.to_string() << ':' << port << '\n';
 
-    try {
-        for (;;){
-        tcp::socket socket{ioc};
+    std::vector<std::thread> threads;
+    threads.reserve(thread_count - 1); // running one thread in main as well
 
-        acceptor.accept(socket);
+    std::shared_ptr<Listener> listener =
+        std::make_shared<Listener>(ioc, ssl_ctx, tcp::endpoint{address, port});
+    listener->run();
 
-        std::thread{
-            [ssl_ctx = std::ref(ssl_ctx), socket = std::move(socket)]() mutable
-            {
-                namespace beast = boost::beast;
-                namespace http = beast::http;
-
-                beast::ssl_stream<tcp::socket&> stream{socket, ssl_ctx.get()};
-
-                beast::error_code ec;
-                bool close = false;
-
-                stream.handshake(net::ssl::stream_base::server, ec);
-                if (ec)
-                    throw_exception(exception_type::unnamed_yet, "SSL handshake failed");
-                
-                beast::flat_buffer buffer;
-                http::request<http::string_body> req;
-                http::read(stream, buffer, req, ec);
-                if (ec)
-                    throw_exception(exception_type::unnamed_yet, ec.message());
-                http::response<http::string_body> res{http::status::ok, req.version()};
-
-                // Send the response
-                http::write(stream, res, ec);
-                if (ec)
-                    throw_exception(exception_type::unnamed_yet, "HTTP write failed");
-            }
-        }.detach();
-        }
-    } catch (const std::exception& e) {
-        std::cerr << "Exception: " << e.what() << '\n';
-        exit(EXIT_FAILURE);
+    for (uint i = 0; i < thread_count - 1; i++)
+    {
+        threads.emplace_back([&ioc](){
+            ioc.run();
+        });
     }
+
+    ioc.run();
+
+    return EXIT_SUCCESS;
 }
